906737414611f2e2cae8aa1256ad94dda56c8dc4
lebohang0824 <lebohang0824@gmail.com>
2020-05-19 20:30:26 +0200

    Add slide

--- js/main.js
+++ js/main.js
@@ -5,4 +5,38 @@ $(document).ready(function(){
     $('.venobox').venobox({
         share: []
     }); 
+
+
+// Slick
+$('.work').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+        {
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+        }
+    ]
+});
 });
