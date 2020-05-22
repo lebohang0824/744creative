93462a7bbc550f1619307e8803c3c69ce7e03d33
lebohang0824 <lebohang0824@gmail.com>
2020-05-22 07:40:31 +0200

    little update

--- js/main.js
+++ js/main.js
@@ -6,37 +6,45 @@ $(document).ready(function(){
         share: []
     }); 
 
+    // Nav
+    $('.burger').click(function(e) {
+        if ($('ul').css('display') == 'none') {
+            $('ul').addClass('mobile');
+        } else {
+            $('ul').removeClass('mobile');
+        }
+    });
 
-// Slick
-$('.work').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    responsive: [
-        {
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-        }
-        },
-        {
-        breakpoint: 600,
-        settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-        }
-        },
-        {
-        breakpoint: 480,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-        }
-        }
-    ]
-});
+    // Slick
+    $('.work').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+        ]
+    });
 });
